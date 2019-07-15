e692d5d849f79082c311d576d275bed22e665405
gyyxtx <1042766239@qq.com>
2019-07-15 08:43:48 +0800

    筛选条件布局完成

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,44 +1,49 @@
 <template>
-    <div class="article-container">
-        <!-- 筛选区域 -->
-        <el-card>
-            <div slot="header">
-                <!-- 面包屑导航 -->
-                <el-breadcrumb separator-class="el-icon-arrow-right">
-                    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                    <el-breadcrumb-item>内容管理</el-breadcrumb-item>
-                </el-breadcrumb>
-            </div>
-            <!-- 筛选项 reqParams提交的参数 -->
-            <el-form :model="reqParams">
-                <el-form-item label="状态:">
-                    <template>
-                        <el-radio-group v-model="reqParams.status">
-                        <el-radio :label='null'>全部</el-radio>
-                        <el-radio :label="0">草稿</el-radio>
-                        <el-radio :label="1">待审核</el-radio>
-                        <el-radio :label="2">审核通过</el-radio>
-                        <el-radio :label="3">审核失败</el-radio>
-                        </el-radio-group>
-                    </template>
-                </el-form-item>
-                <el-form-item label="频道:">
-                    <template>
-                        <el-select v-model="reqParams.channel_id" placeholder="请选择">
-                            <el-option
-                            v-for="item in channelOptions"
-                            :key="item.id"
-                            :label="item.name"
-                            :value="item.id">
-                            </el-option>
-                        </el-select>
-                    </template>
-                </el-form-item>
-            </el-form>
-        </el-card>
-        <!-- 筛选结果区域 -->
-        <el-card></el-card>
-    </div>
+  <div class="article-container">
+    <!-- 筛选区域 -->
+    <el-card>
+      <div slot="header">
+        <!-- 面包屑导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <!-- 筛选项 reqParams提交的参数 -->
+      <el-form :model="reqParams">
+        <el-form-item label="状态:">
+            <el-radio-group v-model="reqParams.status">
+              <el-radio :label="null">全部</el-radio>
+              <el-radio :label="0">草稿</el-radio>
+              <el-radio :label="1">待审核</el-radio>
+              <el-radio :label="2">审核通过</el-radio>
+              <el-radio :label="3">审核失败</el-radio>
+            </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道:">
+            <el-select v-model="reqParams.channel_id" placeholder="请选择">
+              <el-option
+                v-for="item in channelOptions"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              ></el-option>
+            </el-select>
+        </el-form-item>
+        <el-form-item label="时间:">
+            <el-date-picker
+              v-model="dateValues"
+              type="daterange"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+            ></el-date-picker>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- 筛选结果区域 -->
+    <el-card></el-card>
+  </div>
 </template>
 
 <script>
@@ -47,17 +52,21 @@ export default {
     return {
       reqParams: {
         status: null,
-        channel_id: null
+        channel_id: null,
+        begin_pubdate: null,
+        end_pubdate: null
       },
       //  频道选项组数据
-      channelOptions: [{ name: 'java', id: 1 }]
+      channelOptions: [{ name: 'java', id: 1 }],
+      //  日期数据
+      dateValues: []
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-    .el-card {
-        margin-bottom: 20px;
-    }
+.el-card {
+  margin-bottom: 20px;
+}
 </style>
